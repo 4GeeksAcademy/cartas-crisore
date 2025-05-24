16404ede5b55b1cde5310d6526cf86f13ac45247
cristobalorellanav <87153747+cristobalorellanav@users.noreply.github.com>
2025-05-24 18:13:42 +0000

    random-tradingcarts

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  generateRandomCard();
 };
+
+function generateRandomCard() {
+  const cardValues = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+  const cardSuits = [
+    { symbol: '♥', name: 'heart' },
+    { symbol: '♠', name: 'spade' },
+    { symbol: '♣', name: 'club' },
+    { symbol: '♦', name: 'diamond' }
+  ];
+
+  const selectedValue = cardValues[Math.floor(Math.random() * cardValues.length)];
+  const selectedSuit = cardSuits[Math.floor(Math.random() * cardSuits.length)];
+
+  document.querySelector('.top-suit').textContent = selectedSuit.symbol;
+  document.querySelector('.bottom-suit').textContent = selectedSuit.symbol;
+  document.querySelector('.card-value').textContent = selectedValue;
+
+  document.querySelector('.card').className = 'card mx-auto mt-5 ' + selectedSuit.name;
+}
